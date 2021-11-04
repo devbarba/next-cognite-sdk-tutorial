2ea569772a252b16ce4d4a5b50dda9f2e6da492d
Minion <harishkrishnan@Harish.local>
2021-11-04 02:05:59 -0300

    styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -68,7 +68,7 @@ const Home: NextPage = () => {
             <table id='assets'>
                 <tr key={0}>
                     <th>ID</th>
-                    <th>Asset</th>
+                    <th>Assets</th>
                     <th>Description</th>
                 </tr>
                 {
@@ -95,7 +95,6 @@ const Home: NextPage = () => {
 
             <CogPage>
                 <CogHeaderFull label={'Assets'} icon={'fas fa-box'}>{}</CogHeaderFull>
-
                 <CogRoundedContainer>
                     <CogTable
                         lastPage={lastPage}
@@ -124,41 +123,6 @@ const Home: NextPage = () => {
                 </CogRoundedContainer>
             </CogPage>
         </>
-        // <div
-        //     style={{
-        //         display: 'flex',
-        //         flexDirection: 'column',
-        //         justifyContent: 'center',
-        //         alignItems: 'center'
-        //     }}
-        // >
-        //     <div
-        //         style={{
-        //             width: '100%',
-        //             marginBottom: '2rem',
-        //             background: '#CCC',
-        //             display: 'flex',
-        //             justifyContent: 'center'
-        //         }}
-        //     >
-        //         <input
-        //             type="text"
-        //             value={search}
-        //             placeholder='Search by the asset name'
-        //             onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-        //                 setSearch(e.target.value);
-        //                 handleSearch();
-        //             }}
-        //             style={{
-        //                 width: '40%'
-        //             }}
-        //         />
-        //     </div>
-
-        //     <div>
-        //         {assets && assets.length > 0 && renderAssets()}
-        //     </div>
-        // </div>
     )
 }
 
